ae8e3f2a97c1491f272ea0e900cf8dfa1e9673ba
kwaaan <68629563+kwanyun@users.noreply.github.com>
2022-06-02 14:48:37 +0900

    does not die for now

--- Tetris/Game.cpp
+++ Tetris/Game.cpp
@@ -1,75 +1,67 @@
 #include "Game.h"
 
- Application::Application(std::string name)
-	:userName(name), currentTime(time(NULL)) {
-	 for (int i = 0; i < 12; i++)
-	 {
-		 for (int j = 0; j < 10; j++)
-		 {
-			 if (j == 0 || j == 9) map[i][j] = 'l';
-			 else if (i == 11) map[i][j] = '_';
-			 else map[i][j] = ' ';
-		 }
-	 }
- }
+Application::Application(std::string name)
+	:userName(name), currentTime(std::chrono::steady_clock::now()) {
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (j == 0 || j == 11) map[i][j] = 'l';
+			else if (i == 11) map[i][j] = '-';
+			else map[i][j] = ' ';
+		}
+	}
+}
 
 void Application::Run()
 {
 	//random block init
-	Block* currentBlock= Application::GenBlock(currentTime % 5);
-	blockGenerated = true;
+	Block* currentBlock = Application::GenBlock(0);
 	while (true)
 	{
 		//move block
-		if (currentTime - time(NULL) > 0)
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()- currentTime).count() > 1000)
 		{
-			needUpdate = true;
-			currentTime = time(NULL);
+			currentTime = std::chrono::steady_clock::now();
 			//if block current, move down
 			if (blockGenerated)
 			{
-				//get block one step down
-				if (CheckBlock(currentBlock, currentBlock->curR+1, currentBlock->curC ))
-				{
-
-				}
-				else 
-				{
-					currentBlock->curR++;
-				}
+				//move block one step down if possible
+				BlockMove(currentBlock, 1, 0);
 			}
-			//if block is not generated, generate
+			//if block is not generated yet, generate
 			else
 			{
-				currentBlock = Application::GenBlock(currentTime%5);
+				currentBlock = Application::GenBlock(3);
 			}
 		}
 
 		//check key
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000)
 		{
-			needUpdate = true;
+			BlockMove(currentBlock, 0, -1);
 		}
-		else if(GetAsyncKeyState(VK_RIGHT) & 0x8000)
-		{ 
-			needUpdate = true;
-		}
-		else if (GetAsyncKeyState(VK_UP) & 0x8000)
+		else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
 		{
-			needUpdate = true;
+			BlockMove(currentBlock, 0, 1);
 		}
 		else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
 		{
-			needUpdate = true;
-
+			BlockMove(currentBlock, 1, 0);
+		}
+		else if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+		{
+			//RotateBlock(currentBlock);
 		}
 		else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
 		{
-			needUpdate = true;
+			isOver = true;
 		}
+
 		//check end
 		if (isOver)
 		{
+			system("cls");
 			std::cout << "Game END" << std::endl;
 			return ;
 		}
@@ -86,39 +78,100 @@ void Application::Run()
 void Application::Render()
 {
 	system("cls");
+	std::cout << std::endl << std::endl;
 	for (int i = 0; i < 12; i++)
 	{
-		for (int j = 0; j < 10; j++)
+		std::cout << "\t";
+		for (int j = 0; j < 12; j++)
 		{
 			std::cout << map[i][j];
 		}
+		if (i == 5)
+		{
+			std::cout << "\t\t" << "User Name : " << userName;
+		}
+		else if (i == 6)
+		{
+			std::cout << "\t\t" << "Your Score : " << score;
+		}
 		std::cout << std::endl;
 	}
+	std::cout<<std::endl << "Press ESC to end"<<std::endl;
 };
-
+//random generation of the block with time
 Block* Application::GenBlock(unsigned int genNum)
 {
 	Block* mBlock = new Block(genNum);
-	//random generation of the block with time
-
+	blockGenerated = true;
+	needUpdate = true;
+	
 	if (!CheckBlock(mBlock))
 	{
 		isOver = true;
 		delete mBlock;
+		return NULL;
 	}
 	else
-	return mBlock;
+		return mBlock;
 }
 
-bool Application::CheckBlock(Block* theBlock , int leftx , int upy )
+void Application::AddBlockToMap(Block* currentBlock)
+{
+	for (int i = 0; i < currentBlock->rows; i++)
+	{
+		for (int j = 0; j < currentBlock->columns; j++)
+		{
+			if ((map[i+ currentBlock->curR][j + currentBlock->curC] == ' ') && (currentBlock->blockIn[3 * i + j] == '@'))
+				map[i + currentBlock->curR][j + currentBlock->curC] = '@';
+		}
+	}
+}
+
+void Application::DelBlockFromMap(Block* currentBlock)
+{
+	for (int i = 0; i < currentBlock->rows; i++)
+	{
+		for (int j = 0; j < currentBlock->columns; j++)
+		{
+			if ((map[i + currentBlock->curR][j + currentBlock->curC] == '@') && (currentBlock->blockIn[3 * i + j] == '@'))
+				map[i + currentBlock->curR][j + currentBlock->curC] = ' ';
+		}
+	}
+}
+
+
+bool Application::CheckBlock(Block* theBlock , int row , int col )
 {
 	for (int i = 0; i < theBlock->rows; i++)
 	{
 		for (int j = 0; j < theBlock->columns; j++)
 		{
-			if (map[leftx + i][upy + j] != ' ' || theBlock->blockIn[i + 3 * j] != ' ')
+			if ((map[theBlock->curR + row + i][theBlock->curC + col + j] != ' ' && theBlock->blockIn[3 * (i + row) + (j + col)] != '@')
+					&& theBlock->blockIn[3 * i + j] != ' ')
 				return false;
 		}
 	}
+	return true;
 	
+}
+
+void Application::BlockMove(Block* theBlock, int row, int col)
+{
+	needUpdate = true;
+	//move block one step if possible
+	if (CheckBlock(theBlock,  row,  col))
+	{
+		DelBlockFromMap(theBlock);
+		theBlock->curR = theBlock->curR + row;
+		theBlock->curC = theBlock->curC + col;
+		AddBlockToMap(theBlock);
+	}
+	else
+	{
+		//Add Block to map for rendering
+		AddBlockToMap(theBlock);
+		//release memory
+		delete theBlock;
+		blockGenerated = false;
+	}
 }
--- Tetris/Game.h
+++ Tetris/Game.h
@@ -5,21 +5,25 @@
 #include <time.h>
 #include "Block.h"
 #include <windows.h>
-
+#include <chrono>
+#include <thread>
 class Application
 {
 private:
 	std::string userName;
 	unsigned int score=0;
-	unsigned int currentTime = 0;
-	char map[12][10];
+	std::chrono::steady_clock::time_point currentTime;
+	char map[12][12];
 	bool blockGenerated=false;
 	bool needUpdate = false;
 	bool isOver = false;
 public:
-	Application(std::string = "anonymous user");
+	Application(std::string ="anonymous user");
 	void Run();
 	void Render();
 	Block* GenBlock(unsigned int genNum);
+	void AddBlockToMap(Block* currentBlock);
+	void DelBlockFromMap(Block* currentBlock);
+	void BlockMove(Block* theBlock, int leftx = 0, int upy = 0);
 	bool CheckBlock(Block* theBlock ,int leftx=0, int upy=0 );
 };
